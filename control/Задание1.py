c3b675c9a451482fa329fede181999ea1dd63e3f
ekaterinazenkova <31881636+ekaterinazenkova@users.noreply.github.com>
2017-12-12 13:25:17 +0300

    Update Задание1.py

--- control/Задание1.py
+++ control/Задание1.py
@@ -1,3 +1,4 @@
+#with open("/Users/ekaterinazenkova/Desktop/control_languages.txt", 'r', encoding="utf-8") as f: (этот путь был на моём компьютере)
 with open("control_languages.txt", 'r', encoding="utf-8") as f:
     print("Задание1")
     line = f.readline()
