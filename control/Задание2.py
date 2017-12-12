37730a37fb9efcbf5347d182fb3e1efe02a49725
ekaterinazenkova <31881636+ekaterinazenkova@users.noreply.github.com>
2017-12-12 13:30:05 +0300

    Add files via upload

--- control/Задание2.py
+++ control/Задание2.py
@@ -1,14 +1,12 @@
-#with open("/Users/ekaterinazenkova/Desktop/control_languages.txt", 'r', encoding="utf-8") as f: (этот путь был на моём компьютере)
-with open("control_languages.txt", 'r', encoding="utf-8") as f:
+with open("/Users/ekaterinazenkova/Desktop/control_languages.txt", 'r', encoding="utf-8") as f:
     print("Задание2")
     line = f.readline()
     status = "Critically endangered"
     count = 0
     while line:
-        line = line.split(" ")
-        word = line[2]
+        line = line.rsplit("    ")
+        word = line[0]
         if status in word:
             count = count + 1
-        line = f.resdline()
+        line = f.readline()
     print("В списке", count,"языков сo статусом Critically endangered.")
-        
