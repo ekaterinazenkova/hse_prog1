bded286d170328cf295bcd08e51cb5606c520383
ekaterinazenkova <31881636+ekaterinazenkova@users.noreply.github.com>
2018-01-23 00:03:48 +0300

    Add files via upload

---
+++ hw6/Хайку.py
@@ -0,0 +1,11 @@
+
+import random
+def get_word(filename):
+    word=""
+    with open ("/Users/ekaterinazenkova/Desktop/"+filename, "r", encoding="utf-8") as f:
+        word = f.readline()
+        word = word.split()
+        word = random.choice(word)
+        return word
+print(get_word("Line_1_verb.txt")+" "+get_word("Line_1_prep.txt")+" "+get_word("Line_1_noun.txt")+'\n'+get_word("Line_2_verb.txt")+" "+get_word("Line_2_prep.txt")+" "+get_word("Line_2_noun.txt")+'\n'+get_word("Line_3_verb.txt")+" "+get_word("Line_3_noun.txt"))
+
